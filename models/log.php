870227da87ac576c3610069449d1b018d5a05ee9
Crozet Mathieu <matcrid@hotmail.fr>
2018-12-20 12:31:12 +0100

    poo
    vrai dossiers

--- models/log.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$post = print_r($_POST, true);
-file_put_contents("post_form.log", $post);
--- models/test.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-$lines = file('post_form.log'); // display file line by line 
-var_dump($lines);
